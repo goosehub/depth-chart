a3187b4fd1b02572945724ecb106f7eea7c9cebf
Goose <goosemixes@gmail.com>
2024-10-24 12:37:35 -0400

    Team images and team select

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # depth-chart
- My take on the Draft Sharks depth charts
+
+My take on the Draft Sharks depth charts
+
+## Important Note
+
+This project makes use of modules, so [CORS will block requests if opened from disk](https://stackoverflow.com/a/72906725/3774582). To view in a browser either place on a server or visit this project at [https://gooseweb.io/depth-chart/](https://gooseweb.io/depth-chart/).
+
+## Technical Requirements
+
+- Using a no build tools Vue.js via CDN
+- Using plain CSS with Flexbox, no frameworks
+- Semantic HTML
+
+## Features
+
+- Team Selection
+- Dark Mode
--- main-component.js
+++ main-component.js
@@ -1,18 +1,240 @@
 import { ref } from 'vue'
 export default {
-  setup() {
-  },
-  template: `
-  <div class="heading">
-    <h1>Hello World</h1>
-    <div class="blur"></div>
-  </div>
-  <p>
-    Welcome to this page.
-  </p>
-  <test></test>
-  <a href="https://github.com/goosehub/depth-chart" target="_blank" aria-label="Github Link" class="btn">
-    Link to this project on Github
-  </a>
-  `
+    setup() {
+    },
+    mounted() {
+        this.currentTeam = this.teamInfo.cardinals
+    },
+    data() {
+        return {
+            currentTeam: null,
+            darkMode: false,
+            teamInfo: {
+                cardinals: {
+                    fullName: 'Arizona Cardinals',
+                    cityName: 'Arizona',
+                    teamName: 'Cardinals',
+                    image: 'cardinals',
+                },
+                falcons: {
+                    fullName: 'Atlanta Falcons',
+                    cityName: 'Atlanta',
+                    teamName: 'Falcons',
+                    image: 'falcons',
+                },
+                ravens: {
+                    fullName: 'Baltimore Ravens',
+                    cityName: 'Baltimore',
+                    teamName: 'Ravens',
+                    image: 'ravens',
+                },
+                bills: {
+                    fullName: 'Buffalo Bills',
+                    cityName: 'Buffalo',
+                    teamName: 'Bills',
+                    image: 'bills',
+                },
+                panthers: {
+                    fullName: 'Carolina Panthers',
+                    cityName: 'Carolina',
+                    teamName: 'Panthers',
+                    image: 'panthers',
+                },
+                bears: {
+                    fullName: 'Chicago Bears',
+                    cityName: 'Chicago',
+                    teamName: 'Bears',
+                    image: 'bears',
+                },
+                bengals: {
+                    fullName: 'Cincinnati Bengals',
+                    cityName: 'Cincinnati',
+                    teamName: 'Bengals',
+                    image: 'bengals',
+                },
+                browns: {
+                    fullName: 'Cleveland Browns',
+                    cityName: 'Cleveland',
+                    teamName: 'Browns',
+                    image: 'browns',
+                },
+                cowboys: {
+                    fullName: 'Dallas Cowboys',
+                    cityName: 'Dallas',
+                    teamName: 'Cowboys',
+                    image: 'cowboys',
+                },
+                broncos: {
+                    fullName: 'Denver Broncos',
+                    cityName: 'Denver',
+                    teamName: 'Broncos',
+                    image: 'broncos',
+                },
+                lions: {
+                    fullName: 'Detroit Lions',
+                    cityName: 'Detroit',
+                    teamName: 'Lions',
+                    image: 'lions',
+                },
+                packers: {
+                    fullName: 'Green Bay Packers',
+                    cityName: 'Green Bay',
+                    teamName: 'Packers',
+                    image: 'packers',
+                },
+                texans: {
+                    fullName: 'Houston Texans',
+                    cityName: 'Houston',
+                    teamName: 'Texans',
+                    image: 'texans',
+                },
+                colts: {
+                    fullName: 'Indianapolis Colts',
+                    cityName: 'Indianapolis',
+                    teamName: 'Colts',
+                    image: 'colts',
+                },
+                jaguars: {
+                    fullName: 'Jacksonville Jaguars',
+                    cityName: 'Jacksonville',
+                    teamName: 'Jaguars',
+                    image: 'jaguars',
+                },
+                chiefs: {
+                    fullName: 'Kansas City Chiefs',
+                    cityName: 'Kansas City',
+                    teamName: 'Chiefs',
+                    image: 'chiefs',
+                },
+                raiders: {
+                    fullName: 'Las Vegas Raiders',
+                    cityName: 'Las Vegas',
+                    teamName: 'Raiders',
+                    image: 'raiders',
+                },
+                chargers: {
+                    fullName: 'Los Angeles Chargers',
+                    cityName: 'Los Angeles',
+                    teamName: 'Chargers',
+                    image: 'chargers',
+                },
+                rams: {
+                    fullName: 'Los Angeles Rams',
+                    cityName: 'Los Angeles',
+                    teamName: 'Rams',
+                    image: 'rams',
+                },
+                dolphins: {
+                    fullName: 'Miami Dolphins',
+                    cityName: 'Miami',
+                    teamName: 'Dolphins',
+                    image: 'dolphins',
+                },
+                vikings: {
+                    fullName: 'Minnesota Vikings',
+                    cityName: 'Minnesota',
+                    teamName: 'Vikings',
+                    image: 'vikings',
+                },
+                patriots: {
+                    fullName: 'New England Patriots',
+                    cityName: 'New England',
+                    teamName: 'Patriots',
+                    image: 'patriots',
+                },
+                saints: {
+                    fullName: 'New Orleans Saints',
+                    cityName: 'New Orleans',
+                    teamName: 'Saints',
+                    image: 'saints',
+                },
+                giants: {
+                    fullName: 'New York Giants',
+                    cityName: 'New York',
+                    teamName: 'Giants',
+                    image: 'giants',
+                },
+                jets: {
+                    fullName: 'New York Jets',
+                    cityName: 'New York',
+                    teamName: 'Jets',
+                    image: 'jets',
+                },
+                eagles: {
+                    fullName: 'Philadelphia Eagles',
+                    cityName: 'Philadelphia',
+                    teamName: 'Eagles',
+                    image: 'eagles',
+                },
+                steelers: {
+                    fullName: 'Pittsburgh Steelers',
+                    cityName: 'Pittsburgh',
+                    teamName: 'Steelers',
+                    image: 'steelers',
+                },
+                fortyniners: {
+                    fullName: 'San Francisco 49ers',
+                    cityName: 'San Francisco',
+                    teamName: '49ers',
+                    image: 'fortyniners',
+                },
+                seahawks: {
+                    fullName: 'Seattle Seahawks',
+                    cityName: 'Seattle',
+                    teamName: 'Seahawks',
+                    image: 'seahawks',
+                },
+                buccaneers: {
+                    fullName: 'Tampa Bay Buccaneers',
+                    cityName: 'Tampa Bay',
+                    teamName: 'Buccaneers',
+                    image: 'buccaneers',
+                },
+                titans: {
+                    fullName: 'Tennessee Titans',
+                    cityName: 'Tennessee',
+                    teamName: 'Titans',
+                    image: 'titans',
+                },
+                commanders: {
+                    fullName: 'Washington Commanders',
+                    cityName: 'Washington',
+                    teamName: 'Commanders',
+                    image: 'commanders',
+                },
+            }
+        }
+    },
+    methods: {
+        toggleDarkMode() {
+            this.darkMode = !this.darkMode
+        },
+    },
+    template: `
+    <section class="root_section" :class="darkMode ? 'dark_mode' : ''">
+        <div class="heading">
+            <h1 class="depth_chart_header">Team Depth Charts</h1>
+            <div class="blur"></div>
+        </div>
+        <div v-if="currentTeam">
+        <h2 class="team_name">{{currentTeam.fullName}}</h2>
+        <img class="team_logo" :src="'images/' + currentTeam.image + '.gif'" :alt="currentTeam.fullName"/>
+        <select class="team_select" v-model="currentTeam">
+                <option v-for="team in teamInfo" :value="team">
+                {{team.fullName}}
+            </option>
+            </select>
+        </div>
+        <button @click="toggleDarkMode()">
+            Switch to {{darkMode ? 'Light' : 'Dark'}} Mode
+        </button>
+        <p>
+            Welcome to this page.
+        </p>
+        <test></test>
+        <a href="https://github.com/goosehub/depth-chart" target="_blank" aria-label="Github Link" class="btn">
+            Link to this project on Github
+        </a>
+    </section>
+    `
 }
