f48168840c6e3372624f86bc51eba7b2c16d6e51
Goose <goosemixes@gmail.com>
2024-10-24 10:55:09 -0400

    Set up to use modules, components

---
+++ main-component.js
@@ -0,0 +1,18 @@
+import { ref } from 'vue'
+export default {
+  setup() {
+  },
+  template: `
+  <div class="heading">
+    <h1>Hello World</h1>
+    <div class="blur"></div>
+  </div>
+  <p>
+    Welcome to this page.
+  </p>
+  <test></test>
+  <a href="https://github.com/goosehub/depth-chart" target="_blank" aria-label="Github Link" class="btn">
+    Link to this project on Github
+  </a>
+  `
+}
