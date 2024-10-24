cc674d767f32150e230d53b88a126f9ed250467e
Goose <goosemixes@gmail.com>
2024-10-24 18:35:21 -0400

    Create navigation

--- README.md
+++ README.md
@@ -6,13 +6,16 @@ My take on the Draft Sharks depth charts
 
 ## Important Note
 
-This project makes use of modules, so [CORS will block requests if opened from disk](https://stackoverflow.com/a/72906725/3774582). To view in a browser either place on a server or visit this project at [https://gooseweb.io/depth-chart/](https://gooseweb.io/depth-chart/).
+This project makes use of modules, so [CORS will block requests if opened from disk](https://stackoverflow.com/a/72906725/3774582). To view in a browser either host on a server or visit this project at [https://gooseweb.io/depth-chart/](https://gooseweb.io/depth-chart/).
 
 ## Technical Requirements
 
 - Using a no build tools Vue.js via CDN
 - Using plain CSS with Flexbox, no frameworks
 - Semantic HTML
+- Abstraction of CSS color scheme
+- Keyboard navigation of top level inputs
+- Accessibility for screen readers with aria-label and alt tags
 
 ## Features
 
@@ -20,7 +23,3 @@ This project makes use of modules, so [CORS will block requests if opened from d
 - Filtering by Offense, Defense, and Position Groups
 - Independent sorting of positional players
 - Dark Mode
-- Flexbox based CSS grid and abstraction of CSS color scheme
-- Clean semantic HTML
-- Keyboard navigation of top level inputs
-- Accessibility for screen readers with aria-label and alt tags
--- main-component.js
+++ main-component.js
@@ -2,6 +2,21 @@ import { ref } from 'vue'
 export default {
     template: `
     <section class="root_section" :class="darkMode ? 'dark_mode' : ''">
+        <div class="navigation">
+            <button class="btn" @click="toggleDarkMode()">
+                Switch to {{darkMode ? 'Light' : 'Dark'}} Mode
+            </button>
+            <a href="https://github.com/goosehub/depth-chart" target="_blank" aria-label="Link to Github">
+                <button class="btn">
+                    View On Github
+                </button>
+            </a>
+            <a class="cory" href="https://gooseweb.io/resume/" target="_blank" aria-label="Link to Resume">
+                <button class="btn">
+                    By Cory Long
+                </button>
+            </a>
+        </div>
         <h1 class="depth_chart_header">Team Depth Charts</h1>
         <div v-if="currentTeam">
             <div class="team_selection_parent">
@@ -83,14 +98,6 @@ export default {
             </div>
         </div>
         <test></test>
-        <button class="btn" @click="toggleDarkMode()">
-            Switch to {{darkMode ? 'Light' : 'Dark'}} Mode
-        </button>
-        <a href="https://github.com/goosehub/depth-chart" target="_blank" aria-label="Link to Github">
-            <button class="btn">
-                View On Github
-            </button>
-        </a>
     </section>
     `,
     methods: {
