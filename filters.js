6c07218133c4dc63c642ba854671ee14e528ec17
Goose <goosemixes@gmail.com>
2024-10-24 20:13:14 -0400

    Minor style tweaks

--- filters.js
+++ filters.js
@@ -4,53 +4,53 @@ export default {
         <div class="filters" aria-label="Filters">
             <button class="btn" :class="show.offense ? 'active' : 'inactive'" @click="toggleShow('offense')">
                 Offense
-                <span v-if="show.offense" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.offense" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.defense ? 'active' : 'inactive'" @click="toggleShow('defense')">
                 Defense
-                <span v-if="show.defense" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.defense" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.qb ? 'active' : 'inactive'" @click="toggleShow('qb')">
                 QB
-                <span v-if="show.qb" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.qb" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.rb ? 'active' : 'inactive'" @click="toggleShow('rb')">
                 RB
-                <span v-if="show.rb" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.rb" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.wr ? 'active' : 'inactive'" @click="toggleShow('wr')">
                 WR
-                <span v-if="show.wr" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.wr" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.te ? 'active' : 'inactive'" @click="toggleShow('te')">
                 TE
-                <span v-if="show.te" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.te" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.k ? 'active' : 'inactive'" @click="toggleShow('k')">
                 K
-                <span v-if="show.k" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.k" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.dl ? 'active' : 'inactive'" @click="toggleShow('dl')">
                 DL
-                <span v-if="show.dl" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.dl" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.lb ? 'active' : 'inactive'" @click="toggleShow('lb')">
                 LB
-                <span v-if="show.lb" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.lb" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
             <button class="btn" :class="show.db ? 'active' : 'inactive'" @click="toggleShow('db')">
                 DB
-                <span v-if="show.db" aria-label="Visible, select to hide">✓</span>
-                <span v-else aria-label="Hidden, select to show">X</span>
+                <span class="show_toggle" v-if="show.db" aria-label="Visible, select to hide">✓</span>
+                <span class="show_toggle" v-else aria-label="Hidden, select to show">-</span>
             </button>
         </div>
     `,
